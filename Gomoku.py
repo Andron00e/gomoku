c45efb91fef023fa3f0db2b8c8f250454b914d9c
Andron00e <semenov.andrei.v@gmail.com>
2023-06-10 16:24:57 +0300

    samples of game have been added

---
+++ Gomoku.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+class Background():
+    pass
+
+class Menu(Background):
+    pass
+
+class Board():
+    def __init__(self, args*):
+        pass
+
+    def draw_board(self):
+        for horizont in range(15):
+            '''
+            start_pixel_x = 
+            start_pixel_y = 
+            end_pixel_x = 
+            end_pixel_y = 
+            NAMING ???
+            '''
+        for vert in range(15):
+            '''
+            start_pixel_x = 
+            start_pixel_y = 
+            end_pixel_x = 
+            end_pixel_y = 
+            NAMING ???
+            '''
+        pass
+
+    pass
+
+class Game():
+    pass
